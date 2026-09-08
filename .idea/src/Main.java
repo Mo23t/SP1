6f51ab430df27f86ca6d3228b2af39b7db3a20ba
Mohamed <Mohamedawees19@gmail.com>
2026-09-08 15:10:10 +0200

    first push

---
+++ .idea/src/Main.java
@@ -0,0 +1,43 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import java.util.Arrays;
+
+public class Main {
+
+    void main() {
+        String Herobuilder = "Ragnar";
+        int Healthpoints = 85;
+        Healthpoints = 55;
+        int Maxhealth = 100;
+        int level = 5;
+        int Exp = 2300;
+        double Gold = (double)156.5F;
+        boolean Alive = true;
+        String[] var10000 = new String[]{"W(Warrior)", "M(Mage)", "R(Rogue)"};
+        String Inv = "Iventory (6 items): ";
+        String sw = "-Sword";
+        String sh = "-Shield";
+        String pot = "-Potion";
+        String[] items = new String[]{"bow", "gun", "magic"};
+        String Status = "Ready to level up!";
+        String living = "Ragnar is still alive";
+        System.out.println(Herobuilder);
+        System.out.println(Healthpoints);
+        System.out.println(Maxhealth);
+        System.out.println(level);
+        System.out.println(Exp);
+        System.out.println(Gold);
+        System.out.println(Alive);
+        System.out.println(Inv);
+        System.out.println(sw);
+        System.out.println(sh);
+        System.out.println(pot);
+        System.out.println(Arrays.toString(items));
+        System.out.println(Status);
+        System.out.println(Healthpoints);
+        System.out.println(living);
+    }
+}
